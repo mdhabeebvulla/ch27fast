67141a96ae278f465e091a5d1f2decd75fff72c4
mdhabeebvulla <55741151+mdhabeebvulla@users.noreply.github.com>
2026-09-19 11:32:03 +0530

    Enhance predict_iris function with docstring
    Add docstring to predict_iris function and fix typos.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import joblib
 app = FastAPI()
 @app.get("/")
 def predict_iris(sl:float,sw:float,pl:float,pw:float):
+  """functtion to predict IRIS data"""
   model = joblib.load("mymodel")
   result = model.predict([[sl,sw,pl,pw]])
   return {"predcition is ":int(result[0])}
